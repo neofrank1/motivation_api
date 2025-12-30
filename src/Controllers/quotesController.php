62259103dc97080dcca252a070e5f097c600b680
neofrankuy21 <neofrankuy21@gmail.com>
2025-12-30 22:58:32 +0800

    Add test for api Quotes

--- src/Controllers/quotesController.php
+++ src/Controllers/quotesController.php
@@ -6,10 +6,28 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 class QuotesController {
 
-    public function test(Request $request, Response $response): bool
+    public function getQuotes(Request $request, Response $response) {
+
+        if ($request->getMethod() !== 'GET') {
+            $response->getBody()->write(json_encode(["error" => "Method Not Allowed"]));
+            return $response->withStatus(405)->withHeader('Content-Type', 'application/json');
+        }
+        
+        $response->getBody()->write(json_encode([
+            ["quote" => "The only limit to our realization of tomorrow is our doubts of today.", "author" => "Franklin D. Roosevelt"],
+            ["quote" => "In the middle of every difficulty lies opportunity.", "author" => "Albert Einstein"],
+            ["quote" => "What you get by achieving your goals is not as important as what you become by achieving your goals.", "author" => "Zig Ziglar"]
+        ]));
+        $response->withHeader('Content-Type', 'application/json');
+        
+        return $response;
+    }
+
+    public function test(Request $request, Response $response)
     {
-       echo "Quotes Controller is working!";
-       return true;
+       $response->getBody()->write("Quotes Controller is working!");
+       
+       return $response;
     }
 
 }
--- src/Routes/routes.php
+++ src/Routes/routes.php
@@ -4,4 +4,5 @@ use Controllers\QuotesController;
 
 return function($app) {
     $app->get('/', [QuotesController::class, 'test']);
+    $app->get('/quotes', [QuotesController::class, 'getQuotes']);
 };
